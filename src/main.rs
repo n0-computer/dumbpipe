df7f40975270e1e6b255bb2c7bfc9fa8616ea7f4
Ruediger Klaehn <rklaehn@protonmail.com>
2023-12-02 22:38:19 +0200

    Add ForwardTcp mode which just forwards incoming dumbpipes to a host:port via TCP

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use clap::{Parser, Subcommand};
 use iroh_net::{MagicEndpoint, NodeAddr};
 use serde::{Deserialize, Serialize};
 use std::{fmt::Display, io, str::FromStr};
+use tokio::io::{AsyncRead, AsyncWrite};
 use tokio_util::sync::CancellationToken;
 
 const ALPN: &[u8] = b"DUMBPIPEV0";
@@ -18,6 +19,7 @@ pub struct Args {
 pub enum Commands {
     Listen(ListenArgs),
     Connect(ConnectArgs),
+    ForwardTcp(ForwardTcpArgs),
 }
 
 /// A token containing everything to get a file from the provider.
@@ -96,6 +98,20 @@ pub struct ListenArgs {
 }
 
 #[derive(Parser, Debug)]
+pub struct ForwardTcpArgs {
+    #[clap(long)]
+    pub host: Option<String>,
+
+    /// The port to forward to.
+    #[clap(long, default_value_t = 0)]
+    pub port: u16,
+
+    /// The secret key to use. Random by default.
+    #[clap(long)]
+    pub secret: Option<iroh_net::key::SecretKey>,
+}
+
+#[derive(Parser, Debug)]
 pub struct ConnectArgs {
     /// The node to connect to
     pub ticket: NodeTicket,
@@ -109,26 +125,26 @@ pub struct ConnectArgs {
     pub secret: Option<iroh_net::key::SecretKey>,
 }
 
-async fn forward_from_stdin(
+async fn forward_from(
+    mut from: impl AsyncRead + Unpin,
     mut send: quinn::SendStream,
     token: CancellationToken,
 ) -> anyhow::Result<()> {
-    let mut stdin = tokio::io::stdin();
     tokio::select! {
-        _ = tokio::io::copy(&mut stdin, &mut send) => {}
+        _ = tokio::io::copy(&mut from, &mut send) => {}
         _ = token.cancelled() => {}
     }
     send.finish().await?;
     Ok(())
 }
 
-async fn forward_to_stdout(
+async fn forward_to(
     mut recv: quinn::RecvStream,
+    mut to: impl AsyncWrite + Unpin,
     token: CancellationToken,
 ) -> anyhow::Result<()> {
-    let mut stdout = tokio::io::stdout();
     tokio::select! {
-        _ = tokio::io::copy(&mut recv, &mut stdout) => {}
+        _ = tokio::io::copy(&mut recv, &mut to) => {}
         _ = token.cancelled() => {
             recv.stop(0u8.into())?;
         }
@@ -136,16 +152,21 @@ async fn forward_to_stdout(
     Ok(())
 }
 
-async fn forward_stdio(send: quinn::SendStream, recv: quinn::RecvStream) -> anyhow::Result<()> {
+async fn forward_bidi(
+    from1: impl AsyncRead + Send + Sync + Unpin + 'static,
+    to1: impl AsyncWrite + Send + Sync + Unpin + 'static,
+    from2: quinn::RecvStream,
+    to2: quinn::SendStream,
+) -> anyhow::Result<()> {
     let token1 = CancellationToken::new();
     let token2 = token1.clone();
     let token3 = token1.clone();
     let forward_from_stdin = tokio::spawn(async move {
-        forward_from_stdin(send, token1.clone()).await.ok();
+        forward_from(from1, to2, token1.clone()).await.ok();
         token1.cancel();
     });
     let forward_to_stdout = tokio::spawn(async move {
-        forward_to_stdout(recv, token2.clone()).await.ok();
+        forward_to(from2, to1, token2.clone()).await.ok();
         token2.cancel();
     });
     let _control_c = tokio::spawn(async move {
@@ -193,7 +214,7 @@ async fn listen(args: ListenArgs) -> anyhow::Result<()> {
         let mut buf = [0u8; 5];
         r.read_exact(&mut buf).await?;
         anyhow::ensure!(&buf == b"hello", "invalid hello");
-        forward_stdio(s, r).await?;
+        forward_bidi(tokio::io::stdin(), tokio::io::stdout(), r, s).await?;
         break;
     }
     Ok(())
@@ -214,7 +235,56 @@ async fn connect(args: ConnectArgs) -> anyhow::Result<()> {
     // the connecting side must write first. we don't know if there will be something
     // on stdin, so just write a handshake.
     s.write_all(b"hello").await?;
-    forward_stdio(s, r).await?;
+    forward_bidi(tokio::io::stdin(), tokio::io::stdout(), r, s).await?;
+    Ok(())
+}
+
+async fn forward_tcp(args: ForwardTcpArgs) -> anyhow::Result<()> {
+    let secret_key = args.secret.unwrap_or_else(|| {
+        let res = iroh_net::key::SecretKey::generate();
+        eprintln!("using secret key {}", res);
+        res
+    });
+    let endpoint = MagicEndpoint::builder()
+        .alpns(vec![ALPN.to_vec()])
+        .secret_key(secret_key)
+        .bind(args.port)
+        .await?;
+    // wait for the endpoint to figure out its address before making a ticket
+    while endpoint.my_derp().is_none() {
+        tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+    }
+    let addr = endpoint.my_addr().await?;
+    let ticket = NodeTicket { addr };
+    eprintln!(
+        "Forwarding incoming requests to port {}. Use e.g. http://{}.localhost:8100 to connect.",
+        args.port, ticket.addr.node_id,
+    );
+
+    while let Some(connecting) = endpoint.accept().await {
+        let Ok(connection) = connecting.await else {
+            // if accept fails, we want to continue accepting connections
+            continue;
+        };
+        let Ok((s, mut r)) = connection.accept_bi().await else {
+            // if accept_bi fails, we want to continue accepting connections
+            continue;
+        };
+        println!("got connection");
+        // read the handshake and verify it
+        let mut buf = [0u8; 5];
+        r.read_exact(&mut buf).await?;
+        anyhow::ensure!(&buf == b"hello", "invalid hello");
+        let host = args.host.clone().unwrap_or_else(|| "localhost".to_string());
+        println!("got handshake");
+        let port = args.port;
+        tokio::spawn(async move {
+            let connection = tokio::net::TcpStream::connect((host, port)).await?;
+            let (read, write) = connection.into_split();
+            forward_bidi(read, write, r, s).await?;
+            anyhow::Ok(())
+        });
+    }
     Ok(())
 }
 
@@ -224,6 +294,7 @@ async fn main() -> anyhow::Result<()> {
     let res = match args.command {
         Commands::Listen(args) => listen(args).await,
         Commands::Connect(args) => connect(args).await,
+        Commands::ForwardTcp(args) => forward_tcp(args).await,
     };
     match res {
         Ok(()) => std::process::exit(0),
